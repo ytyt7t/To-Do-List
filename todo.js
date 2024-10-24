bdae54f669a33b1a040e24638a611ba04006185f
Santhosh.s <110976748+ytyt7t@users.noreply.github.com>
2024-10-24 13:50:04 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,35 @@
+const inputBox=document.getElementById("input-box");
+const listConatiner=document.getElementById("listcontainer");
+
+function addTask()
+{
+    if(inputBox.value === '')
+    {
+        alert("you must write something !");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listConatiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerhtml='\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value=" ";
+}
+list-Conatiner.addEventListener("click",function(e)
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+    }
+}.false);
+
+function saveData()
+{
+   localStorage.setItem("data",list-Conatiner.innerHTML)
+}
